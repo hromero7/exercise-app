3dab5ab85c4b6d88cc2cac045e33140e6cf6e605
hrome <58020216+hromero7@users.noreply.github.com>
2020-08-27 20:57:42 -0700

    heroku push

---
+++ exercise-tracker/client/build/precache-manifest.a5b1fdae9cead50c237c0fa02f1a84d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed8bff0fbb4718febae9bd9cce4ae732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37b53602de7390f8930a",
+    "url": "/static/css/2.b56a0318.chunk.css"
+  },
+  {
+    "revision": "5734663b29453eb1e19e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "37b53602de7390f8930a",
+    "url": "/static/js/2.e043d380.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.e043d380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5734663b29453eb1e19e",
+    "url": "/static/js/main.0c2195a0.chunk.js"
+  },
+  {
+    "revision": "666df64c05a4cb398c37",
+    "url": "/static/js/runtime-main.d66d7b0a.js"
+  }
+]);
